695fe9432fc44c4b3dc43c01738721f2efa26264
ArthurLVDH <squishylxix@gmail.com>
2025-02-01 15:27:48 -0800

    made chatwindowcomp push messages and periodically pull

--- IceBreaker-frontend/src/lib/components/ChatWindowComponent.svelte
+++ IceBreaker-frontend/src/lib/components/ChatWindowComponent.svelte
@@ -2,8 +2,12 @@
     import type {ChatMessageEntry} from "$lib";
 
     let {chatTarget} = $props();
+    let message = $state("");
 
     let messages: ChatMessageEntry[] = $state([]);
+    setInterval(() => {
+        messages = getMessages();
+    }, 1000);
 
     // I'll implement these two later
     // treat them as placeholder to interact with the backend
@@ -23,6 +27,14 @@
             <p>{message.content}</p>
         </div>
     {/each}
+</div>
+
+<div>
+    <input type="text" placeholder="Type your message here" bind:value={message}/>
+    <button onclick={() => {
+        sendMessage(message);
+        message = "";
+    }}>Send</button>
 </div>
 
 <p>Chat Window</p>
