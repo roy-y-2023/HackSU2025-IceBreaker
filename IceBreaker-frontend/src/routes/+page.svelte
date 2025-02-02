e7fa9358d35c413f5d3deaca1f851cb9c05ddcf9
ioSquishy <squishylxix@gmail.com>
2025-02-02 09:12:32 -0800

    home page with login and register buttons

--- IceBreaker-frontend/src/routes/+page.svelte
+++ IceBreaker-frontend/src/routes/+page.svelte
@@ -1,2 +1,33 @@
-<h1>Welcome to IceBreaker</h1>
+<script lang="ts">
+    function login() {
+        
+    }
 
+    function register() {
+        
+    }
+</script>
+
+<style>
+    * {
+        display: flex;
+    }
+
+    .centered {
+        justify-content: center;
+        text-align: center;
+    }
+
+    .button {
+        margin: 10px;
+    }
+</style>
+
+<h1 class="centered">Welcome to IceBreaker</h1>
+
+<div class="centered">
+    <div>
+        <button class="button" on:click={login}>Login</button>
+        <button class="button" on:click={register}>Register</button>
+    </div>
+</div>
