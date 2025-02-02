a5025ed027119895776b5cd6449b7ade43f9af9f
ioSquishy <squishylxix@gmail.com>
2025-02-01 16:37:47 -0800

    added tag functionality

--- IceBreaker-frontend/src/routes/interests/+page.svelte
+++ IceBreaker-frontend/src/routes/interests/+page.svelte
@@ -1,17 +1,42 @@
+<!-- in this page the users can setup there tags -->
+
 <script lang="ts">
     let tags: string[] = $state([]);
 
-    function getTags(): string[] {
+    let tag = $state("");
 
+    function getTags(): string[] {
+        return tags;
     }
 
     function addTag(tag: string){
-
+        if (tag == "" || tags.includes(tag)) return;
+        tags.push(tag);
     }
 
     function removeTag(tag: string){
-
+        tags.find((t, index) => {
+            if (t == tag){
+                tags.splice(index, 1);
+            }
+        });
     }
 </script>
 
-<!-- in this page the users can setup there tags -->
+<div>
+    {#each tags as tag}
+        <div style="display: flex;">
+            <p>{tag}</p>
+            <button onclick={() => removeTag(tag)}>Remove</button>
+        </div>
+    {/each}
+</div>
+
+<div>
+    <input type="text" placeholder="Type your tag here" bind:value={tag}/>
+    <button onclick={() => {
+        addTag(tag);
+        tag = "";
+    }}>Add</button>
+</div>    
+
