89ad1abf407cd42fdbb46c125fb72976dc96ed8d
ioSquishy <squishylxix@gmail.com>
2025-02-02 10:12:05 -0800

    added register page

---
+++ IceBreaker-frontend/src/routes/register/+page.svelte
@@ -0,0 +1,84 @@
+<script lang="ts">
+    import {page} from "$app/state";
+
+    import {iceBreakerAccessToken} from "$lib/store";
+
+    let loginSuccess = false;
+    if (page.url.searchParams.has("success")) {
+        loginSuccess = true;
+        window.history.replaceState({}, document.title, "/login");
+    }
+
+    function registerSubmit() {
+
+    }
+</script>
+
+{#if loginSuccess}
+    <p class="has-text-centered">Login Success!</p>
+{/if}
+
+<style>
+    @import '../global.css';
+
+    * {
+        display: flex;
+        flex-wrap: wrap;
+    }
+
+    .form {
+        justify-items: left;
+        flex-direction: column;
+    }
+
+</style>
+
+<div class="centered" style="flex-direction: column;">
+    <a class="button" href="./login/discord">Login with Discord</a>
+</div>
+
+<hr>
+
+<div class="centered form">
+    <div class="field">
+        <div class="control has-icons-left has-icons-right">
+            <input class="input is-success" type="text" placeholder="Username">
+            <span class="icon is-small is-left">
+                <i class="fas fa-user"></i>
+            </span>
+            <span class="icon is-small is-right">
+                <i class="fas fa-check"></i>
+            </span>
+        </div>
+    </div>
+
+    <div class="field">
+        <div class="control has-icons-left has-icons-right">
+            <input class="input is-danger" type="email" placeholder="Email">
+            <span class="icon is-small is-left">
+                <i class="fas fa-envelope"></i>
+            </span>
+            <span class="icon is-small is-right">
+                <i class="fas fa-exclamation-triangle"></i>
+            </span>
+        </div>
+    </div>
+
+    <div class="field">
+        <p class="control has-icons-left">
+            <input class="input" type="password" placeholder="Password">
+            <span class="icon is-small is-left">
+            <i class="fas fa-lock"></i>
+            </span>
+        </p>
+    </div>
+
+    <div class="field is-grouped">
+        <div class="control">
+            <button class="button is-link" onclick={registerSubmit}>Submit</button>
+        </div>
+        <div class="control">
+            <button class="button is-link is-light">Cancel</button>
+        </div>
+    </div>
+</div>
